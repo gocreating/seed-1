88558d000be250fd5725fc8f971e2f9212271fe8
gocreating <gocreating@gmail.com>
2015-04-16 22:33:35 +0800

    refine gulpfile and change livereload to browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
-/*!
- * gulp
- * $ npm install gulp-less gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-livereload del --save-dev
+/**
+ * Load plugins
  */
- 
-// Load plugins
 var gulp = require('gulp'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
@@ -13,83 +10,112 @@ var gulp = require('gulp'),
     // imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    // cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync'),
+    nodemon = require('gulp-nodemon'),
+    changed = require('gulp-changed'),
     del = require('del');
- 
-// Nodejs
-gulp.task('nodejs', function() {
-  return gulp.src(['src/**/*', '!src/assets'])
-    .pipe(gulp.dest('build/debug'))
-    .pipe(livereload())
-    /*.pipe(notify({ message: 'Nodejs task complete' }))*/;
+
+/**
+ * clean the build files
+ */
+gulp.task('clean', function(cb) {
+    del(['build/debug/'], cb);
 });
 
-// Styles
+/**
+ * compile .less files
+ */
 gulp.task('styles', function() {
   return gulp.src('src/assets/less/main.less')
+    .pipe(changed('build/debug/assets/css'))
     .pipe(less())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('build/debug/assets/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('build/debug/assets/css'))
-    .pipe(livereload())
-    /*.pipe(notify({ message: 'Styles task complete' }))*/;
+    .pipe(gulp.dest('build/debug/assets/css'));
 });
  
-// Scripts
+/**
+ * minify front-end .js files
+ */
 gulp.task('scripts', function() {
   return gulp.src('src/assets/js/**/*.js')
+    .pipe(changed('build/debug/assets/js'))
     // .pipe(jshint('.jshintrc'))
     // .pipe(jshint.reporter('default'))
     // .pipe(concat('main.js'))
     .pipe(gulp.dest('build/debug/assets/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('build/debug/assets/js'))
-    .pipe(livereload())
-    /*.pipe(notify({ message: 'Scripts task complete' }))*/;
+    .pipe(gulp.dest('build/debug/assets/js'));
 });
  
-// Images
+/**
+ * compressing images
+ */
 gulp.task('images', function() {
   return gulp.src('src/assets/img/**/*')
+    .pipe(changed('build/debug/assets/img'))
     // .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('build/debug/assets/img'))
-    .pipe(livereload())
-    /*.pipe(notify({ message: 'Images task complete' }))*/;
+    .pipe(gulp.dest('build/debug/assets/img'));
 });
  
-// Clean
-gulp.task('clean', function(cb) {
-    // del(['build/debug/assets/css', 'build/debug/assets/js', 'build/debug/assets/img'], cb);
-    del(['build/debug/'], cb);
+/**
+ * copy files that does not need to be preprocessed,
+ * like nodejs server files, controllers, models, etc.
+ */
+gulp.task('copy', function() {
+  return gulp.src(['src/**/*', '!src/assets/'])
+    .pipe(changed('build/debug'))
+    .pipe(gulp.dest('build/debug'));
 });
  
-// Watch
+/**
+ * watch and reprocess files that changed
+ */
 gulp.task('watch', function() {
-  // Create LiveReload server
-  livereload.listen();
-
-  // Watch .scss files
+  // watch .less files
   gulp.watch('src/assets/less/**/*.less', ['styles']);
  
-  // Watch .js files
+  // watch .js files
   gulp.watch('src/assets/js/**/*.js', ['scripts']);
  
-  // Watch image files
+  // watch image files
   gulp.watch('src/assets/img/**/*', ['images']);
  
-  // Watch other files
-  gulp.watch(['src/**/*', '!src/assets/**/*'], ['nodejs']);
-
-  // Watch any files in dist/, reload on change
-  // gulp.watch(['build/debug/**']).on('change', livereload.changed);
+  // watch other files
+  gulp.watch(['src/**/*', '!src/assets/**/*'], ['copy']);
 });
 
-// Synchronize database
+
+/**
+ * run the server
+ */
+gulp.task('nodemon', function (cb) {
+  return nodemon({
+    script: 'build/debug/app.js'
+  }).on('start', function () {
+    cb();
+  });
+});
+
+/**
+ * livereload and synchronize the browser operations
+ */
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: 'localhost:4000',
+    files: ['build/debug/**/*.*'],
+    port: 7000,
+  });
+});
+
+/**
+ * synchronize database
+ * however, this task is still not working
+ * please ignore it
+ */
 gulp.task('syncdb', function() {
   var orm = require("orm");
   orm.connect("sqlite://db.sqlite", function (err, db) {
@@ -102,7 +128,9 @@ gulp.task('syncdb', function() {
   });
 });
 
-// Default task
+/**
+ * default task
+ */
 gulp.task('default', ['clean'], function() {
-    gulp.start('nodejs', 'styles', 'scripts', 'images', 'watch');
+    gulp.start('copy', 'styles', 'scripts', 'images', 'watch', 'browser-sync');
 });
