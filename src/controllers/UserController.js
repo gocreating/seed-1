3949938c8c36eea449504f54a124fbda165dadf0
gocreating <gocreating@gmail.com>
2015-04-28 18:24:06 +0800

    register function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,30 @@
 module.exports = {
   register: {
     get: function(req, res) {
-      res.render('user/register');
-    },
-    post: function(req, res) {
-      var newUser = {};
-      newUser.username = req.body.username;
-      newUser.password = req.body.password;
-      newUser.isVerified = false;
-      req.models.user.create(newUser, function(err, results) {
-        console.log('new user');
-        res.redirect('/user/login');
+      res.render('user/register', {
+        msg: ''
       });
+    },
+    post: function(req, res, next) {
+      var newUser = {
+        username: req.body.username,
+        password: req.body.password,
+        isVerified: false,
+      };
+      req.models.user.register(newUser, function(err, isExist, user) {
+        if (err) return next(err);
+        if (isExist) {
+          res.render('user/register', {
+            msg: 'User already exist'
+          });
+        } else{
+          res.redirect('/user/login');
+        }
+      });
+      // req.models.user.create(newUser, function(err, results) {
+      //   console.log('new user');
+      //   res.redirect('/user/login');
+      // });
     }
   },
   login: {
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -10,4 +10,29 @@ module.exports = function(orm, db) {
     openId:     {type: 'integer'},
     socialData: {type: 'object'},
   });
+
+  /**
+   * Register a new user
+   * @param {object}   newUser                - The user object that will be created
+   * @param {function} cb(err, isExist, user) - The callback function
+   * @param {boolean}  cb().isExist           - Whether the user is already exitsting
+   * @param {object}   cb().user              - The user object just created
+   */
+  User.register = function(newUser, cb) {
+    User.exists({username: newUser.username}, function(err, isExist) {
+      if (err) return cb(err, true, null);
+      if (isExist) {
+        return cb(null, true, null);
+      } else {
+        User.create(newUser, function(err, results) {
+          if (err) return cb(err, true, null);
+          return cb(null, false, results);
+        });
+      }
+    });
+  };
+
+  User.login = function(email, password) {
+    
+  };
 };
