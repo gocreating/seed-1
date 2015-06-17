ee73ea4681bb5e94d72f663d59ddae7e1b6e6cf9
Go Creating <gocreating@GodeMacBook-Air.local>
2015-06-17 10:59:22 +0800

    add more paths to test

--- src/test/index.js
+++ src/test/index.js
@@ -1,17 +1,26 @@
-var assert = require('chai').assert;
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
 var request = require('supertest');
 
 describe('User Module', function() {
   describe('Routing', function() {
-    it('should respond to GET /', function(done) {
-      request('http://localhost:5000/')
-        .get()
-        .end(function(err, res) {
-          expect(res).to.not.be.undefined;
-          expect(res.status).to.equal(200);
-          done();
-        });
+    var paths = [
+      '/user/register',
+      '/user/login',
+      '/user/logout',
+      '/user/profile'
+    ];
+
+    paths.forEach(function(path) {
+      it('should respond to GET ' + path, function(done) {
+        request('http://localhost:5000' + path)
+          .get()
+          .end(function(err, res) {
+            expect(res).to.not.be.undefined;
+            expect(res.status).to.equal(200);
+            done();
+          });
+      });
     });
   });
 });
